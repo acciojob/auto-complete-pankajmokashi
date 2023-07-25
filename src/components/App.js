8038b3c46411c3af807fbb6d5018655d0205b315
pankajmokashi <pankajmokasi12@gmail.com>
2023-07-25 18:11:30 +0530

    submitting code-Tue Jul 25 2023 18:11:27 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import './../styles/App.css'
+
+const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
 
 const App = () => {
+
+  let [searched, setSearched] = useState("")
+  let [filteredData, setFilteredData] = useState(fruits)
+
+  useEffect(() =>{
+    searched == "" ? setFilteredData(fruits) :
+    (setFilteredData(fruits.filter((ele) => ele.includes(searched))))
+  }, [searched])
+  
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Search Item</h1>
+        <input type="text" onChange={(e) => setSearched(e.target.value)} value={searched}></input>
+        <ul>
+          {
+            filteredData.length > 0 &&
+            filteredData.map(item => (
+              <li>{item}</li>
+            ))
+          }
+        </ul>
     </div>
   )
 }
